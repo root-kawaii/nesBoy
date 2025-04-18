f8ac7c4946781fd8526ac1776a4d58e3cedb19b2
Matteo Regge <reggematteo@gmail.com>
2025-04-19 01:56:35 +0200

    first commit

---
+++ build/CMakeFiles/cpu_6502.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpu_6502.dir/main.cpp.obj"
+  "CMakeFiles/cpu_6502.dir/main.cpp.obj.d"
+  "cpu_6502.exe"
+  "cpu_6502.exe.manifest"
+  "cpu_6502.pdb"
+  "libcpu_6502.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpu_6502.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cpu_6502.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cpu_6502.
+# This may be replaced when dependencies are built.
